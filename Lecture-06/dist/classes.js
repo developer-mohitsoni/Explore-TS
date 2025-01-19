f9fd22f4a6d751ad1f7724d6f940a009684aa051
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-19 16:40:33 +0530

    Add example of class implementation and method binding in TypeScript

--- Lecture-06/dist/classes.js
+++ Lecture-06/dist/classes.js
@@ -1,1 +1,17 @@
 "use strict";
+//! Basic Class
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        return `Department Name: ${this.name}`;
+    }
+}
+const accounting = new Department("Accounting");
+console.log(accounting.describe()); // Department Name: Accounting
+const accountingCopy = {
+    name: "HR",
+    describe: accounting.describe,
+};
+console.log(accountingCopy.describe()); // Department Name: HR
