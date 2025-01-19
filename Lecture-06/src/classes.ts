8a8284690d8842a252c591ede6d2a375fbc8ee3d
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-19 17:03:04 +0530

    Refactor Department class to use explicit property declarations and readonly id, removing unnecessary parameters from the constructor

--- Lecture-06/src/classes.ts
+++ Lecture-06/src/classes.ts
@@ -22,14 +22,48 @@
 // console.log(accountingCopy.describe()); // Department Name: HR
 
 //! Access Modifiers
-class Department {
-  //   name: string;
-  //   private employees: string[];
 
-  // shorthand
-  constructor(public name: string, private employees: string[]) {
-    // this.name = n;
-    // this.employees = [];
+// class Department {
+//   //   name: string;
+//   //   private employees: string[];
+
+//   // shorthand
+//   constructor(public name: string, private employees: string[]) {
+//     // this.name = n;
+//     // this.employees = [];
+//   }
+//   describe(this: Department) {
+//     return `Department Name: ${this.name}`;
+//   }
+
+//   addEmployee(employee: string) {
+//     this.employees.push(employee);
+//   }
+
+//   printeEmployee() {
+//     console.log(`Number of employees: ${this.employees.length}`);
+//     console.log(this.employees);
+//   }
+// }
+
+// const accounting = new Department("Accounting", []);
+// console.log(accounting.describe()); // Department Name: Accounting
+// accounting.addEmployee("Mohit");
+// accounting.addEmployee("Ayushi");
+// accounting.printeEmployee(); // Number of employees: 2
+
+// console.log(accounting.employees);
+
+//! readonly
+
+class Department {
+  public name: string;
+  private employees: string[];
+  private readonly id: string;
+  constructor(n: string) {
+    this.name = n;
+    this.id = "d1"; // only once you can initialize
+    this.employees = [];
   }
   describe(this: Department) {
     return `Department Name: ${this.name}`;
@@ -44,11 +78,3 @@ class Department {
     console.log(this.employees);
   }
 }
-
-const accounting = new Department("Accounting", []);
-console.log(accounting.describe()); // Department Name: Accounting
-accounting.addEmployee("Mohit");
-accounting.addEmployee("Ayushi");
-accounting.printeEmployee(); // Number of employees: 2
-
-// console.log(accounting.employees);
