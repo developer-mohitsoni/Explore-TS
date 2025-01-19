8a01f8ed44718f5354b22cbcf4f0f60b20f7a3a1
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-19 14:53:18 +0530

    Refactor add function to remove input validation

--- Lecture-01/app.ts
+++ Lecture-01/app.ts
@@ -1,8 +1,5 @@
 function add(num1: number, num2: number) {
-  if (typeof num1 === "number" && typeof num2 === "number") return num1 + num2;
-  else {
-    throw new Error("Both inputs must be numbers");
-  }
+  return num1 + num2;
 }
 
 const result = add(5, 10);
