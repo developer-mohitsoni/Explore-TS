619074241720d609ef7a4b911b12e6356d44c736
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-19 16:57:17 +0530

    Enhance Department class with employee management methods and private property

--- Lecture-06/dist/classes.js
+++ Lecture-06/dist/classes.js
@@ -1,17 +1,41 @@
 "use strict";
 //! Basic Class
+// class Department {
+//   name: string;
+//   constructor(n: string) {
+//     this.name = n;
+//   }
+//   describe(this: Department) {
+//     return `Department Name: ${this.name}`;
+//   }
+// }
+// const accounting = new Department("Accounting");
+// console.log(accounting.describe()); // Department Name: Accounting
+// const accountingCopy = {
+//   name: "HR",
+//   describe: accounting.describe,
+// };
+// console.log(accountingCopy.describe()); // Department Name: HR
+//! Access Modifiers
 class Department {
     constructor(n) {
         this.name = n;
+        this.employees = [];
     }
     describe() {
         return `Department Name: ${this.name}`;
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printeEmployee() {
+        console.log(`Number of employees: ${this.employees.length}`);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department("Accounting");
 console.log(accounting.describe()); // Department Name: Accounting
-const accountingCopy = {
-    name: "HR",
-    describe: accounting.describe,
-};
-console.log(accountingCopy.describe()); // Department Name: HR
+accounting.addEmployee("Mohit");
+accounting.addEmployee("Ayushi");
+accounting.printeEmployee(); // Number of employees: 2
+// console.log(accounting.employees); 
--- Lecture-06/src/classes.ts
+++ Lecture-06/src/classes.ts
@@ -1,22 +1,53 @@
 //! Basic Class
 
+// class Department {
+//   name: string;
+//   constructor(n: string) {
+//     this.name = n;
+//   }
+//   describe(this: Department) {
+//     return `Department Name: ${this.name}`;
+//   }
+// }
+
+// const accounting = new Department("Accounting");
+
+// console.log(accounting.describe()); // Department Name: Accounting
+
+// const accountingCopy = {
+//   name: "HR",
+//   describe: accounting.describe,
+// };
+
+// console.log(accountingCopy.describe()); // Department Name: HR
+
+//! Access Modifiers
 class Department {
   name: string;
+  private employees: string[];
+
   constructor(n: string) {
     this.name = n;
+    this.employees = [];
   }
   describe(this: Department) {
     return `Department Name: ${this.name}`;
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printeEmployee() {
+    console.log(`Number of employees: ${this.employees.length}`);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
-
 console.log(accounting.describe()); // Department Name: Accounting
+accounting.addEmployee("Mohit");
+accounting.addEmployee("Ayushi");
+accounting.printeEmployee(); // Number of employees: 2
 
-const accountingCopy = {
-  name: "HR",
-  describe: accounting.describe,
-};
-
-console.log(accountingCopy.describe()); // Department Name: HR
+// console.log(accounting.employees); 
