94d2543fa40740fb912f05fa1d846b5e3fe71384
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-19 14:50:59 +0530

    Add input validation to add function in JavaScript and TypeScript implementations

--- Lecture-01/app.js
+++ Lecture-01/app.js
@@ -1,5 +1,9 @@
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number")
+        return num1 + num2;
+    else {
+        throw new Error("Both inputs must be numbers");
+    }
 }
 var result = add(5, 10);
 console.log(result);
--- Lecture-01/app.ts
+++ Lecture-01/app.ts
@@ -1,5 +1,8 @@
 function add(num1: number, num2: number) {
-  return num1 + num2;
+  if (typeof num1 === "number" && typeof num2 === "number") return num1 + num2;
+  else {
+    throw new Error("Both inputs must be numbers");
+  }
 }
 
 const result = add(5, 10);
